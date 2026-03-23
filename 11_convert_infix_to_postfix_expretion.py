086a75f8f713fc65310d19ed0e0aa05ac3550b97
legend048 <pklegend048@gmail.com>
2026-03-23 10:54:59 +0530

    goff code

--- 11_convert_infix_to_postfix_expretion.py
+++ 11_convert_infix_to_postfix_expretion.py
@@ -1,20 +1,16 @@
 def infix_to_postfix(expression):
-    p, s, o, i = {'+': 1, '-': 1, '*': 2, '/': 2, '^': 3}, [], [], 0
-    while i < len(expression):
-        c = expression[i]
-        if c.isspace(): i += 1; continue
-        if c.isalnum():
-            j = i
-            while j < len(expression) and expression[j].isalnum(): j += 1
-            o.append(expression[i:j]); i = j; continue
-        if c == ')':
+    p, s, o, t = {'+': 1, '-': 1, '*': 2, '/': 2, '^': 3}, [], [], ''
+    for c in expression + ' ':
+        if c.isalnum(): t += c; continue
+        if t: o.append(t); t = ''
+        if c.isspace(): continue
+        if c == '(': s.append(c)
+        elif c == ')':
             while s and s[-1] != '(': o.append(s.pop())
             if s: s.pop()
-        elif c == '(': s.append(c)
         else:
             while s and s[-1] != '(' and (p[s[-1]] > p[c] if c == '^' else p[c] <= p[s[-1]]): o.append(s.pop())
             s.append(c)
-        i += 1
     while s: o.append(s.pop())
     return " ".join(o)
 
