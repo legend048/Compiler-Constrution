f370775ca75f8d5d0e2a507390b2f5ce084b45d1
legend048 <pklegend048@gmail.com>
2026-03-23 10:34:31 +0530

    right associative fixing

--- 11_convert_infix_to_postfix_expretion.py
+++ 11_convert_infix_to_postfix_expretion.py
@@ -1,5 +1,6 @@
 def infix_to_postfix(expression):
     precedence = {'+': 1, '-': 1, '*': 2, '/': 2, '^': 3}
+    right_associative = {'^'}
     stack = []
     output = []
     for char in expression:
@@ -12,8 +13,12 @@ def infix_to_postfix(expression):
                 output.append(stack.pop())
             stack.pop()
         else:
-            while stack and stack[-1] != '(' and precedence.get(char, 0) <= precedence.get(stack[-1], 0):
-                output.append(stack.pop())
+            if char in right_associative:
+                while stack and stack[-1] != '(' and precedence.get(stack[-1], 0) > precedence.get(char, 0):
+                    output.append(stack.pop())
+            else:
+                while stack and stack[-1] != '(' and precedence.get(char, 0) <= precedence.get(stack[-1], 0):
+                    output.append(stack.pop())
             stack.append(char)
     while stack:
         output.append(stack.pop())
