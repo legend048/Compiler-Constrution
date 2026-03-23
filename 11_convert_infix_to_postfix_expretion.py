c2b45b1b0c151c21e12de84762969a48e183d241
legend048 <pklegend048@gmail.com>
2026-03-23 10:49:59 +0530

    multi num support adding

--- 11_convert_infix_to_postfix_expretion.py
+++ 11_convert_infix_to_postfix_expretion.py
@@ -1,19 +1,22 @@
 def infix_to_postfix(expression):
-    prec = {'+': 1, '-': 1, '*': 2, '/': 2, '^': 3}
-    stack, output = [], []
-    for char in expression:
-        if char.isalnum(): output.append(char)
-        elif char == '(': stack.append(char)
-        elif char == ')':
-            while stack and stack[-1] != '(': output.append(stack.pop())
-            stack.pop()
+    p, s, o, i = {'+': 1, '-': 1, '*': 2, '/': 2, '^': 3}, [], [], 0
+    while i < len(expression):
+        c = expression[i]
+        if c.isspace(): i += 1; continue
+        if c.isalnum():
+            j = i
+            while j < len(expression) and expression[j].isalnum(): j += 1
+            o.append(expression[i:j]); i = j; continue
+        if c == ')':
+            while s and s[-1] != '(': o.append(s.pop())
+            if s: s.pop()
+        elif c == '(': s.append(c)
         else:
-            while stack and stack[-1] != '(' and (prec.get(stack[-1], 0) > prec.get(char, 0)
-            if char == '^' else prec.get(char, 0) <= prec.get(stack[-1], 0)):
-                output.append(stack.pop())
-            stack.append(char)
-    while stack: output.append(stack.pop())
-    return "".join(output)
+            while s and s[-1] != '(' and (p[s[-1]] > p[c] if c == '^' else p[c] <= p[s[-1]]): o.append(s.pop())
+            s.append(c)
+        i += 1
+    while s: o.append(s.pop())
+    return " ".join(o)
 
 user_input = input("Enter Infix Expression: ")
 print("Postfix Expression: ", infix_to_postfix(user_input))
