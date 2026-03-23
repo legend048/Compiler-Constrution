30336332ae62bde8e06d48012cd3d56d95f69333
legend048 <pklegend048@gmail.com>
2026-03-23 10:40:44 +0530

    code made shorter

--- 11_convert_infix_to_postfix_expretion.py
+++ 11_convert_infix_to_postfix_expretion.py
@@ -1,27 +1,18 @@
 def infix_to_postfix(expression):
-    precedence = {'+': 1, '-': 1, '*': 2, '/': 2, '^': 3}
-    right_associative = {'^'}
-    stack = []
-    output = []
+    prec = {'+': 1, '-': 1, '*': 2, '/': 2, '^': 3}
+    stack, output = [], []
     for char in expression:
-        if char.isalnum():
-            output.append(char)
-        elif char == '(':
-            stack.append(char)
+        if char.isalnum(): output.append(char)
+        elif char == '(': stack.append(char)
         elif char == ')':
-            while stack and stack[-1] != '(':
-                output.append(stack.pop())
+            while stack and stack[-1] != '(': output.append(stack.pop())
             stack.pop()
         else:
-            if char in right_associative:
-                while stack and stack[-1] != '(' and precedence.get(stack[-1], 0) > precedence.get(char, 0):
-                    output.append(stack.pop())
-            else:
-                while stack and stack[-1] != '(' and precedence.get(char, 0) <= precedence.get(stack[-1], 0):
-                    output.append(stack.pop())
+            while stack and stack[-1] != '(' and (prec.get(stack[-1], 0) > prec.get(char, 0)
+            if char == '^' else prec.get(char, 0) <= prec.get(stack[-1], 0)):
+                output.append(stack.pop())
             stack.append(char)
-    while stack:
-        output.append(stack.pop())
+    while stack: output.append(stack.pop())
     return "".join(output)
 
 user_input = input("Enter Infix Expression: ")
